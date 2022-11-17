3bd3b0433b26538758a047031fe6c112406c9c64
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2022-11-17 12:51:27 +0000

    Update codeblock02.tf

--- codeblock02.tf
+++ codeblock02.tf
@@ -254,7 +254,7 @@ resource "aws_security_group" "dat01" {
     description     = "Data Inbound"
     from_port       = 1421
     to_port         = 1421
-    protocol        = "-1"
+    protocol        = "tcp"
     security_groups = [aws_security_group.app01.id]
     self            = true
   }
