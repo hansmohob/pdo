1f3bd203db9b36ec0204f0eeebcb5b8732e2b4b6
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2022-11-17 18:11:14 +0000

    Update codeblock05.tf

--- codeblock05.tf
+++ codeblock05.tf
@@ -22,7 +22,7 @@ data "aws_iam_policy_document" "websrv" {
   }
 }
 
-resource "aws_s3_bucket_policy" "websrv" {
-  bucket = aws_s3_bucket.websrv.id
-  policy = data.aws_iam_policy_document.websrv.json
+##CORRUPTED##
+##CORRUPTED##
+##CORRUPTED##
 }
