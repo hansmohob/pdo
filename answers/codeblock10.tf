abed785a78894d10689f0dc9a7f2ebd5d7d26eee
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2022-11-18 13:09:21 +0000

    added asg policy

--- answers/codeblock10.tf
+++ answers/codeblock10.tf
@@ -1,4 +1,4 @@
-#EC2 Auto Scaling Group
+# EC2 Auto Scaling Group
 resource "aws_autoscaling_group" "websrv" {
   name                    = format("%s%s%s%s", var.customer_code, "asg", var.environment_code, "websrv01")
   default_cooldown        = 60
@@ -11,5 +11,21 @@ resource "aws_autoscaling_group" "websrv" {
   launch_template {
     id      = aws_launch_template.websrv.id
     version = "$Latest"
+  }
+}
+
+# EC2 Auto Scaling Policy
+resource "aws_autoscaling_policy" "websrv" {
+  name                      = format("%s%s%s%s", var.customer_code, "asp", var.environment_code, "websrv01")
+  policy_type               = "TargetTrackingScaling"
+  autoscaling_group_name    = aws_autoscaling_group.websrv.name
+  estimated_instance_warmup = 100
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 80.0
   }
 }
